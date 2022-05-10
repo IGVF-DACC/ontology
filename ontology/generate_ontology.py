42865fe0514c9885fd485bb8631c7165a2591205
Mingjie Li <mingjiecn@gmail.com>
2022-05-10 15:32:44 -0500

    update scripts

--- ontology/generate_ontology.py
+++ ontology/generate_ontology.py
@@ -1,7 +1,7 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 from urllib.error import HTTPError
-from rdflib import ConjunctiveGraph, exceptions, Namespace
+from rdflib import ConjunctiveGraph, Namespace
 from rdflib import RDFS, RDF, BNode, OWL
 from rdflib.collection import Collection
 import json
@@ -11,6 +11,7 @@ from ontology.ntr_terms import (
     ntr_biosamples
 )
 from ontology.manual_slims import slim_shims
+from ontology.base_slims import base_slims
 
 
 OBO_OWL = Namespace('http://www.geneontology.org/formats/oboInOwl#')
@@ -59,232 +60,40 @@ ONTOLOGY_ASSET_DICT = {
     }
 }
 
-SLIMS_DICT = {
-    'developmental': {
-        'UBERON:0000926': 'mesoderm',
-        'UBERON:0000924': 'ectoderm',
-        'UBERON:0000925': 'endoderm'
-    },
-    'system': {
-        'UBERON:0001015': 'musculature',
-        'UBERON:0000949': 'endocrine system',
-        'UBERON:0002330': 'exocrine system',
-        'UBERON:0000990': 'reproductive system',
-        'UBERON:0001004': 'respiratory system',
-        'UBERON:0001007': 'digestive system',
-        'UBERON:0001008': 'excretory system',
-        'UBERON:0001009': 'circulatory system',
-        'UBERON:0001434': 'skeletal system',
-        'UBERON:0002405': 'immune system',
-        'UBERON:0002416': 'integumental system',
-        'UBERON:0001032': 'sensory system',
-        'UBERON:0001017': 'central nervous system',
-        'UBERON:0000010': 'peripheral nervous system'
-    },
-
-    'organ': {
-        'UBERON:0002369': 'adrenal gland',
-        'UBERON:0002110': 'gallbladder',
-        'UBERON:0002106': 'spleen',
-        'UBERON:0001043': 'esophagus',
-        'UBERON:0000004': 'nose',
-        'UBERON:0000056': 'ureter',
-        'UBERON:0000057': 'urethra',
-        'UBERON:0000059': 'large intestine',
-        'UBERON:0000165': 'mouth',
-        'UBERON:0000945': 'stomach',
-        'UBERON:0000948': 'heart',
-        'UBERON:0000955': 'brain',
-        'UBERON:0000970': 'eye',
-        'UBERON:0000991': 'gonad',
-        'UBERON:0001255': 'urinary bladder',
-        'UBERON:0001264': 'pancreas',
-        'UBERON:0001474': 'bone element',
-        'UBERON:0002048': 'lung',
-        'UBERON:0002097': 'skin of body',
-        'UBERON:0002107': 'liver',
-        'UBERON:0002108': 'small intestine',
-        'UBERON:0002113': 'kidney',
-        'UBERON:0002240': 'spinal cord',
-        'UBERON:0002367': 'prostate gland',
-        'UBERON:0002370': 'thymus',
-        'UBERON:0003126': 'trachea',
-        'UBERON:0001723': 'tongue',
-        'UBERON:0001737': 'larynx',
-        'UBERON:0006562': 'pharynx',
-        'UBERON:0001103': 'diaphragm',
-        'UBERON:0002185': 'bronchus',
-        'UBERON:0000029': 'lymph node',
-        'UBERON:0001132': 'parathyroid gland',
-        'UBERON:0002046': 'thyroid gland',
-        'UBERON:0001981': 'blood vessel',
-        'UBERON:0001473': 'lymphatic vessel',
-        'UBERON:0000178': 'blood',
-        'UBERON:0007844': 'cartilage element',
-        'UBERON:0001690': 'ear',
-        'UBERON:0001987': 'placenta',
-        'UBERON:0001911': 'mammary gland',
-        'UBERON:0000007': 'pituitary gland',
-        'UBERON:0016887': 'extraembryonic component',
-        'UBERON:0001013': 'adipose tissue',
-        'UBERON:0000310': 'breast',
-        'UBERON:0000989': 'penis',
-        'UBERON:0004288': 'skeleton',
-        'UBERON:0000995': 'uterus',
-        'UBERON:0000996': 'vagina',
-        'UBERON:0000992': 'ovary',
-        'UBERON:0000473': 'testis',
-        'UBERON:0003509': 'arterial blood vessel',
-        'UBERON:0001638': 'vein',
-        'UBERON:0000160': 'intestine',
-        'UBERON:0002384': 'connective tissue',
-        'UBERON:0002101': 'limb',
-        'UBERON:0000922': 'embryo',
-        'UBERON:0000383': 'musculature of body',
-        'UBERON:0001021': 'nerve',
-        'UBERON:0002371': 'bone marrow',
-        'UBERON:0006314': 'bodily fluid',
-        'UBERON:0002049': 'vasculature',
-        'UBERON:0000483': 'epithelium',
-        'UBERON:0002407': 'pericardium',
-        'UBERON:0001744': 'lymphoid tissue',
-        'UBERON:0001155': 'colon',
-        'UBERON:0003547': 'brain meninx',
-        'UBERON:0001350': 'coccyx',
-        'UBERON:0002368': 'endocrine gland',
-        'UBERON:0002365': 'exocrine gland',
-        'UBERON:0002073': 'hair follicle',
-        'UBERON:0005057': 'immune organ',
-        'UBERON:0001817': 'lacrimal gland',
-        'UBERON:0002182': 'main bronchus',
-        'UBERON:0001829': 'major salivary gland',
-        'UBERON:0000414': 'mucous gland',
-        'UBERON:0001821': 'sebaceous gland',
-        'UBERON:0000998': 'seminal vesicle',
-        'UBERON:0001820': 'sweat gland',
-        'UBERON:0001471': 'skin of prepuce of penis'
-    },
-
-    'cell': {
-        'CL:0000236': 'B cell',
-        'EFO:0001640': 'B cell',  # B cell derived cell line
-        'EFO:0001639': 'cancer cell',  # cancer cell line
-        'CL:0002494': 'cardiocyte',
-        'CL:0002320': 'connective tissue cell',
-        'CL:0002321': 'embryonic cell',
-        'CL:0000115': 'endothelial cell',
-        'EFO:0005730': 'endothelial cell',  # endothelial cell derived cell line
-        'CL:0000066': 'epithelial cell',
-        'EFO:0001641': 'epithelial cell',  # epithelial cell derived cell line
-        'CL:0000057': 'fibroblast',
-        'EFO:0002009': 'fibroblast',  # fibroblast derived cell line
-        'CL:0000988': 'hematopoietic cell',
-        'EFO:0004905': 'induced pluripotent stem cell',
-        'EFO:0005740': 'induced pluripotent stem cell',  # induced pluripotent stem cell derived cell line
-        'CL:0000312': 'keratinocyte',
-        'CL:0000738': 'leukocyte',
-        'EFO:0005292': 'lymphoblast',  # lymphoblastoid cell line
-        'CL:0000148': 'melanocyte',
-        'CL:0000576': 'monocyte',
-        'CL:0000763': 'myeloid cell',
-        'CL:0000056': 'myoblast',
-        'CL:0002319': 'neural cell',
-        'EFO:0005214': 'neuroblastoma cell',  # neuroblastoma cell line
-        'CL:0000669': 'pericyte',
-        'CL:0000192': 'smooth muscle cell',
-        'EFO:0005735': 'smooth muscle cell',  # smooth muscle cell derived cell line
-        'CL:0000034': 'stem cell',
-        'EFO:0002886': 'stem cell',  # stem cell derived cell line
-        'CL:0000084': 'T cell',
-        'NTR:0000550': 'progenitor cell'
-    },
-
-    'assay': {
-        # Note shortened synonyms are provided
-        'OBI:0000634': 'DNA methylation',  # 'DNA methylation profiling'
-        'OBI:0000424': 'Transcription',  # 'transcription profiling'
-        'OBI:0001398': 'DNA binding',  # "protein and DNA interaction"
-        'OBI:0001854': 'RNA binding',  # "protein and RNA interaction"
-        'OBI:0001917': '3D chromatin structure',  # 'chromosome conformation identification objective'
-        'OBI:0000870': 'DNA accessibility',  # 'single-nucleotide-resolution nucleic acid structure mapping assay'
-        'OBI:0001916': 'Replication timing',
-        'OBI:0000435': 'Genotyping',
-        'OBI:0000615': 'Proteomics',
-        'OBI:0000626': 'DNA sequencing',
-        'OBI:0000845': 'RNA structure',
-        'OBI:0002082': 'Reporter assay',  # 'Reporter gene assay'
-        'OBI:0002675': 'Massively parallel reporter assay',
-        'NTR:0000520': 'CRISPR screen',
-        'OBI:0000711': 'Library preparation',
-        'NTR:0000675': 'Ribosome activity'
-    },
-
-    'preferred_name':  {
-        'OBI:0002117': 'WGS',
-        'OBI:0001247': 'genotyping HTS',
-        'OBI:0001332': 'DNAme array',
-        'OBI:0001335': 'microRNA counts',
-        'OBI:0001463': 'RNA microarray',
-        'OBI:0001863': 'WGBS',
-        'OBI:0001923': 'MS-MS',
-        'OBI:0001271': 'RNA-seq',
-        'OBI:0000716': 'ChIP-seq',
-        'OBI:0001853': 'DNase-seq',
-        'OBI:0001920': 'Repli-seq',
-        'OBI:0001864': 'RAMPAGE',
-        'OBI:0001393': 'genotyping array',
-        'OBI:0002042': 'Hi-C',
-        'OBI:0002457': 'PRO-seq',
-        'OBI:0002458': '4C',
-        'OBI:0002629': 'direct RNA-seq',
-        'OBI:0002144': 'Circulome-seq',
-        'OBI:0002459': 'genotyping HiC',
-        'OBI:0002675': 'MPRA',
-        'OBI:0002571': 'polyA plus RNA-seq',
-        'OBI:0002572': 'polyA minus RNA-seq',
-        'OBI:0002631': 'scRNA-seq',
-        'OBI:0002112': 'small RNA-seq',
-        'OBI:0002083': 'enhancer reporter assay',
-        'OBI:0002762': 'snATAC-seq',
-        'OBI:0002764': 'scATAC-seq',
-        'OBI:0002038': 'Ribo-seq',
-        'OBI:0002984': 'capture Hi-C',
-        'OBI:0003033': 'CUT&RUN',
-        'OBI:0003034': 'CUT&Tag'
-    },
-
-    'category': {
-        'OBI:0000634': 'DNA methylation profiling',
-        'OBI:0000424': 'transcription profiling',
-        'OBI:0000435': 'genotyping',
-        'OBI:0000615': 'proteomics',
-        'OBI:0001916': 'replication',
-        'OBI:0001398': 'protein and DNA interaction',
-        'OBI:0001854': 'protein and RNA interaction'
-    },
-
-    'objective': {
-        'OBI:0000218': 'cellular feature identification objective',
-        'OBI:0001691': 'cellular structure feature identification objective',
-        'OBI:0001916': 'DNA replication identification objective',
-        'OBI:0001917': 'chromosome conformation identification objective',
-        'OBI:0001234': 'epigenetic modification identification objective',
-        'OBI:0001331': 'transcription profiling identification objective',
-        'OBI:0001690': 'molecular function identification objective',
-        'OBI:0000268': 'organism feature identification objective',
-        'OBI:0001623': 'organism identification objective',
-        'OBI:0001398': 'protein and DNA interaction identification objective',
-        'OBI:0001854': 'protein and RNA interaction identification objective'
-    },
-
-    'type': {
-        'OBI:0001700': 'immunoprecipitation assay',
-        'OBI:0000424': 'transcription profiling assay',
-        'OBI:0000634': 'DNA methylation profiling assay',
-        'OBI:0000435': 'genotyping assay'
+PREFERRED_NAME = {
+    'OBI:0002117': 'WGS',
+    'OBI:0001247': 'genotyping HTS',
+    'OBI:0001332': 'DNAme array',
+    'OBI:0001335': 'microRNA counts',
+    'OBI:0001463': 'RNA microarray',
+    'OBI:0001863': 'WGBS',
+    'OBI:0001923': 'MS-MS',
+    'OBI:0001271': 'RNA-seq',
+    'OBI:0000716': 'ChIP-seq',
+    'OBI:0001853': 'DNase-seq',
+    'OBI:0001920': 'Repli-seq',
+    'OBI:0001864': 'RAMPAGE',
+    'OBI:0001393': 'genotyping array',
+    'OBI:0002042': 'Hi-C',
+    'OBI:0002457': 'PRO-seq',
+    'OBI:0002458': '4C',
+    'OBI:0002629': 'direct RNA-seq',
+    'OBI:0002144': 'Circulome-seq',
+    'OBI:0002459': 'genotyping HiC',
+    'OBI:0002675': 'MPRA',
+    'OBI:0002571': 'polyA plus RNA-seq',
+    'OBI:0002572': 'polyA minus RNA-seq',
+    'OBI:0002631': 'scRNA-seq',
+    'OBI:0002112': 'small RNA-seq',
+    'OBI:0002083': 'enhancer reporter assay',
+    'OBI:0002762': 'snATAC-seq',
+    'OBI:0002764': 'scATAC-seq',
+    'OBI:0002038': 'Ribo-seq',
+    'OBI:0002984': 'capture Hi-C',
+    'OBI:0003033': 'CUT&RUN',
+    'OBI:0003034': 'CUT&Tag'
     }
 
-}
 
 
 class Inspector(object):
@@ -385,17 +194,17 @@ def getAncestors(parents, terms, key):
 
 
 def getBaseSlims(term, slimType, slim_candidates):
-    base_slims = []
-    slimTerms = SLIMS_DICT[slimType]
+    base_slim_names = []
+    slimTerms = base_slims[slimType]
     for slimTerm_key in slimTerms:
         if slimTerm_key in slim_candidates:
-            base_slims.append(slimTerms[slimTerm_key])
+            base_slim_names.append(slimTerms[slimTerm_key])
     if slimType in slim_shims:
         # Overrides all Ontology based-slims
         shims_override = slim_shims[slimType].get(term, [])
         if shims_override:
             return shims_override
-    return base_slims
+    return base_slim_names
 
 
 def getTermStructure():
@@ -502,7 +311,7 @@ def main():
                     terms[term_id] = getTermStructure()
                 terms[term_id]['id'] = term_id
                 terms[term_id]['name'] = str(data.rdf_graph.label(c))
-                terms[term_id]['preferred_name'] = SLIMS_DICT['preferred_name'].get(term_id, '')
+                terms[term_id]['preferred_name'] = PREFERRED_NAME.get(term_id, '')
                 # Get all parents
                 for parent in data.get_classDirectSupers(c, excludeBnodes=False):
                     if type(parent) == BNode:
